c0819457fee04e3f9334045d3046779f48eb2efb
Claire Davis <cpv26@cornell.edu>
2016-02-27 23:36:15 -0500

    first commit

---
+++ InterfaceBuilderHelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderHelloWorld
+//
+//  Created by Richie Davis on 2/27/16.
+//  Copyright © 2016 Claire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
